2c9e9cbe34ec53a80846ec93875f3d9bb5614ac0
k3yboardnerd <k3yboardnerd@gmail.com>
2023-08-20 16:05:59 -0700

    Fixing the analyzer

--- analyzer.c
+++ analyzer.c
@@ -31,9 +31,11 @@ int analyzer(const char *format, F_Handler funcs_list[], va_list args_list)
 			}
 			if (!funcs_list[j].symbol && format[i + 1] != ' ')
 			{
-				format[i + 1] != '\0' ?
-					(_output_char(format[i]), _output_char(format[i + 1]),
-					 prntd_chars += 2) : (-1);
+				if (format[i + 1] != '\0')
+				{
+					(_output_char(format[i]), _output_char(format[i + 1]), prntd_chars += 2);
+					return (-1);
+				}
 			}
 			i = i  + 1;
 		}
