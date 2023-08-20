d92d5b33f6f771e5cdd6a00a287b06a64d7d1d43
owen20108 <owen20108@yahoo.com>
2023-08-20 13:10:44 -0700

    main

--- main.h
+++ main.h
@@ -16,12 +16,13 @@ typedef struct Formatter F_Handler;
 int _printf(const char *format, ...);
 int analyzer(const char *format, F_Handler funcs_list[], va_list args_list);
 
-// Custom write function
+
 int _output_char(char c);
 
-// Printing Funcs
+
 int p_char(va_list list);
 int p_string(va_list list);
 int p_percent(attribute((unused)) va_list list);
 
-#endif // MAIN_H
+#endif
+
