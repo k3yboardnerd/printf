1ab6339ef61bd1ec3becd161c8e320060ae0ae80
k3yboardnerd <k3yboardnerd@gmail.com>
2023-08-20 13:17:40 -0700

    Fixed the analyzer

--- analyzer.c
+++ analyzer.c
@@ -1,55 +1,44 @@
 #include "main.h"
+
 /**
  * analyzer - Get main string and all parameters to print format string.
- * @format: string containing all characters.
+ * @format: string have all characters.
  * @funcs_list: list of all the functions.
  * @args_list: list of arguments passed to the program.
  * Return: total characters printed.
  */
+
 int analyzer(const char *format, F_Handler funcs_list[], va_list args_list)
 {
 	int i, j, r_val, prntd_chars;
 
 	prntd_chars = 0;
 
-	for (i = 0; format[i] != '\0'; i++)
+	for (i = 0; format[i]; i++)
 	{
 		if (format[i] == '%')
 		{
-			for (j = 0; funcs_list[j].symbol != NULL; j++)
+			for (j = 0; funcs_list[j].symbol; j++)
 			{
 				if (format[i + 1] == funcs_list[j].symbol[0])
 				{
 					r_val = funcs_list[j].handler(args_list);
 					if (r_val == -1)
-					{
 						return (-1);
-					}
 					prntd_chars += r_val;
 					break;
 				}
 			}
-
-			if (funcs_list[j].symbol == NULL && format[i + 1] != ' ')
+			if (!funcs_list[j].symbol && format[i + 1] != ' ')
 			{
-				if (format[i + 1] != '\0')
-				{
-					_output_char(format[i]);
-					_output_char(format[i + 1]);
-					prntd_chars += 2;
-				}
-				else
-				{
-					return (-1);
-				}
+				prntd_chars += format[i + 1] ?
+					(_output_char(format[i]), _output_char(format[i + 1]), 2) : -1;
 			}
-
-			i += 1;
+			i++;
 		}
 		else
 		{
-			_output_char(format[i]);
-			prntd_chars++;
+			prntd_chars += _output_char(format[i]), 1;
 		}
 	}
 
