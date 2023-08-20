b01f6bffa19a6ffbd3cb923786491a9e2f0229fe
owen20108 <owen20108@yahoo.com>
2023-08-20 12:09:27 -0700

    formatter

---
+++ formatters.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * p_char - Print char
+ * @list: list of args
+ * Return: total chars to be outputed
+ */
+int p_char(va_list list)
+{
+	_output_char(va_arg(list, int));
+	return (1);
+}
+
+/**
+ * p_string - Print all strings
+ * @list: list of args
+ * Return: total chars to be outputed
+ */
+int p_string(va_list list)
+{
+	int i = 0;
+	char *str = va_arg(list, char *);
+
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
+
+	while (str[i] != '\0')
+	{
+		_output_char(str[i]);
+
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * p_percent - Prints percentage symbol
+ * @list: list of args
+ * Return: the % as output
+ */
+int p_percent(attribute((unused)) va_list list)
+{
+	_output_char('%');
+	return (1);
+}
