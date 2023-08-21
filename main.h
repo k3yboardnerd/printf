8c23dd6252c9da62d55c77b646baa7bb8a8b7958
owen20108 <owen20108@yahoo.com>
2023-08-21 12:14:41 -0700

    main updated

--- main.h
+++ main.h
@@ -25,17 +25,14 @@ int analyzer(const char *format, F_Handler funcs_list[], va_list args_list);
 
 int _output_char(char c);
 
-
 int p_char(va_list list);
 int p_string(va_list list);
 int p_percent(__attribute__((unused)) va_list list);
 int p_integer(va_list args);
-int p_uns_num(unsigned int n);
-int p_bin(va_list list);
-char *rev_str(char *s);
 unsigned int b_length(unsigned int num, int b);
-char *_memocpy(char *d, char *src, unsigned int n);
+char *rev_str(char *s);
 void output_base(char *str);
+char *_memocpy(char *d, char *src, unsigned int n);
 int p_bin(va_list list);
 
 #endif
